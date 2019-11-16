5bc4c300e0a78faf3d306526d2c320bc7b37bf7d
Simon-Hwang <952551920@qq.com>
2019-11-17 00:00:37 +0800

    task4

--- Part4/BlusterCritter/src/blustercritter/BlusterCritter.java
+++ Part4/BlusterCritter/src/blustercritter/BlusterCritter.java
@@ -28,20 +28,58 @@ import java.util.ArrayList;
 public class BlusterCritter extends Critter{
 	private static final double FACTOR = 0.05; //darkern coefficient
 	private int c;
-	public BlusterCritter(int cSize)){
+	public BlusterCritter(int cSize){
 		this.c = cSize;
 	}
 	public ArrayList<Actor> getActors(){ 
 		ArrayList<Actor> actors = new ArrayList<Actor>();// get Critter Actor
 		Location loc = getLocation();
 		int row = loc.getRow(), col = loc.getCol();
-		for(int i = row - 2; i <= row += 2; ++i){
-			for(int j = col - 2; j <= col += 2; ++j){ // get actor from 5*5 rectangle
+		int rows = getGrid().getNumRows(), cols = getGrid().getNumCols();
+		int sRow, eRow, sCol, eCol;
+		if(row - 2 >= 0){ // decide the left edge of row
+			sRow = row - 2;
+		}else{
+			if(row - 1 >= 0){
+				sRow = row - 1;
+			}else{
+				sRow = row;	
+			}
+		}
+		if(col - 2 >= 0){ // decide the upper edge of row
+			sCol = col - 2;
+		}else{
+			if(col - 1 >= 0){
+				sCol = col - 1;
+			}else{
+				sCol = col;	
+			}
+		}
+		if(row + 2 < rows){ // decide the right edge of row
+			eRow = row + 2;
+		}else{
+			if(row + 1 < rows){
+				eRow = row + 1;
+			}else{
+				eRow = row;	
+			}
+		}
+		if(col + 2 < cols){ // decide the lowwer edge of row
+			eCol = col + 2;
+		}else{
+			if(col + 1 < cols){
+				eCol = col + 1;
+			}else{
+				eCol = col;	
+			}
+		}
+		for(int i = sRow; i <= eRow; ++i){
+			for(int j = sCol; j <= eCol; ++j){ // get actor from 5*5 rectangle
 				if(i == row && j == row){
 					continue;
 				}
 				Actor a = getGrid().get(new Location(i, j));
-				if (a != NULL && a instanceof Critter){ // record the number of critter, then compare to c
+				if (a != null && a instanceof Critter){ // record the number of critter, then compare to 
 					actors.add(a);
 				}
 			}
@@ -52,17 +90,23 @@ public class BlusterCritter extends Critter{
 	public void processActors(ArrayList<Actor> actors)
     {
         int n = actors.size();
-		Color c = getColor();
-        if (n >= 0){ // if the actors size is zero, the kid will get darken
-			int red = (int) (c.getRed() * (1 - FACTOR));
-			int green = (int) (c.getGreen() * (1 - FACTOR));
-			int blue = (int) (c.getBlue() * (1 - FACTOR));
+		Color color = getColor();
+        if (n >= c){ // if the actors size is zero, the kid will get darken
+			int red = (int) (color.getRed() * (1 - FACTOR));
+			int green = (int) (color.getGreen() * (1 - FACTOR));
+			int blue = (int) (color.getBlue() * (1 - FACTOR));
+			red = red > 0 ? red : 0;
+			green = green > 0? green:0;
+			blue = blue > 0? blue :0;
 			setColor(new Color(red, green, blue));
 			return;
 		}else{//get brighter
-			int red = (int) (c.getRed() * (1 + FACTOR));
-			int green = (int) (c.getGreen() * (1 + FACTOR));
-			int blue = (int) (c.getBlue() * (1 + FACTOR));
+			int red = (int) (color.getRed() * (1 + FACTOR));
+			int green = (int) (color.getGreen() * (1 + FACTOR));
+			int blue = (int) (color.getBlue() * (1 + FACTOR));
+			red = red < 255 ? red : 255;
+			green = green < 255? green:255;
+			blue = blue < 255? blue :255;
 			setColor(new Color(red, green, blue));
 			return;
 		}
--- Part4/BlusterCritter/src/blustercritter/BlusterCritterRunner.java
+++ Part4/BlusterCritter/src/blustercritter/BlusterCritterRunner.java
@@ -18,7 +18,8 @@
 
 package blustercritter; 
 import info.gridworld.actor.ActorWorld;
-import info.gridworld.actor.Rock;
+import info.gridworld.actor.Bug;
+import info.gridworld.actor.Critter;
 import info.gridworld.grid.Location;
 
 import java.awt.Color;
@@ -27,18 +28,23 @@ import java.awt.Color;
  * This class runs a world that contains chameleon critters. <br />
  * This class is not tested on the AP CS A and AB exams.
  */
+class Example extends Bug{
+	public void act(){} //do noting, just stay still
+}
 public final class BlusterCritterRunner
 {
 	private BlusterCritterRunner(){} // constructor
     public static void main(String[] args)
     {
         ActorWorld world = new ActorWorld();
-		world.add(new Location(2,4), new Critter());
-		world.add(new Location(3,5), new Critter());
-		world.add(new Location(4,3), new Critter());
-        world.add(new Location(4, 4), new BlusterCritter());
-        world.add(new Location(5, 8), new BlusterCritter());
-		world.add(new Location(5,7), new Critter());
+		world.add(new Location(2,4), new Example());
+		world.add(new Location(3,5), new Example());
+		world.add(new Location(4,3), new Example());
+        world.add(new Location(4, 4), new BlusterCritter(2));
+        world.add(new Location(5, 6), new BlusterCritter(2));
+		world.add(new Location(5,7), new Example());
+		world.add(new Location(7,2), new Example());
+		world.add(new Location(6,3), new Example());
         world.show();
     }
 }
